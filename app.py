3d3ed4bb50023cd07f7f1ef38a42f7bed15879e3
PatriciaLeitte <pla.alves@uni9.edu.br>
2023-02-14 23:56:25 +0000

    fim da aula 1

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask("__name__")
+app = Flask(__name__)
 
-# POST MOCK
+# POSTS MOCK
 posts = [
     {
         "titulo": "Post 1",
@@ -18,11 +18,6 @@ posts = [
     }
 ]
 
-
 @app.route('/')
 def exibir_entradas():
     return render_template("exibir_entradas.html", entradas=posts)
-
-@app.route('/pudim')
-def pudim():
-    return "<h1>Eu gosto de pudim</h1>"
